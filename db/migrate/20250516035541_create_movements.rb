1bf6ef79f483fd629d3ac131ca0c9220be691f10
TheBonii <Lucasbonino5@hotmail.com>
2025-05-16 00:56:37 -0300

    New: Create migrations and small fixes

---
+++ db/migrate/20250516035541_create_movements.rb
@@ -0,0 +1,4 @@
+class CreateMovements < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
--- server.rb
+++ server.rb
@@ -1,9 +1,10 @@
+require 'sinatra/activerecord'
 require 'sinatra'
 require 'bundler/setup'
 require 'sinatra/reloader' if Sinatra::Base.environment == :development
 
 set :views, File.expand_path('app/views', __dir__)
-set :public, File.expand_path('public', __dir__)
+set :public_folder, File.expand_path('public', __dir__)
 
 class App < Sinatra::Application
   register Sinatra::Reloader
