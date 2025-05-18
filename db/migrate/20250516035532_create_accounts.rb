1ebcfc99fffb8bc0dcb0230b42a0d597e8aecb54
Valentino Vilar <valentinovilar18@gmail.com>
2025-05-18 23:48:13 +0000

    Fix: Add a foreign key to the account table that references the users table, also add the logic in models

--- db/migrate/20250516035532_create_accounts.rb
+++ db/migrate/20250516035532_create_accounts.rb
@@ -4,7 +4,7 @@ class CreateAccounts < ActiveRecord::Migration[8.0]
       t.float :balance
       t.string :cvu
       t.string :alias
-
+      t.references :users, null :false, foreign_key :true
       t.timestamps
     end
   end
