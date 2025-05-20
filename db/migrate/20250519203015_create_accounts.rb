3f220e87b2fbe7ddc517ba2759edab520fd4eb7b
franBarosco <franbarosco@gmail.com>
2025-05-20 19:32:00 +0000

    Fix: Solved the migrations problem for the database
    Signed-off-by: franBarosco <franbarosco@gmail.com>

--- db/migrate/20250519203015_create_accounts.rb
+++ db/migrate/20250519203015_create_accounts.rb
@@ -1,9 +1,9 @@
 class CreateAccounts < ActiveRecord::Migration[8.0]
   def change
     create_table :accounts do |t|
-      t.decimal :balance, precision: 11, scale: 2, default: 0.0, null: false
-      t.string :cvu
-      t.string :alias
+      t.decimal :balance, precision: 11, scale: 2, default: "0.0", null: false
+      t.string :cvu, unique: true
+      t.string :alias, unique: true
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
