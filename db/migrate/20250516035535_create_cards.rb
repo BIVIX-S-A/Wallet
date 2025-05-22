d794a218057e5c380fec5d12f366a7625a0b16d8
FranVesnaver <fevesnaver@gmail.com>
2025-05-21 23:49:51 -0300

    New: added a model for the class Card and a migration for that model

--- db/migrate/20250516035535_create_cards.rb
+++ db/migrate/20250516035535_create_cards.rb
@@ -1,4 +1,16 @@
 class CreateCards < ActiveRecord::Migration[8.0]
   def change
+    create_table :cards do |t|
+      t.string :number, null: false
+      t.date :issue_date
+      t.date :expiry_date
+      t.integer :payment_system, null: false # enum: {visa: 0, mastercard: 1}
+      t.string :cvv
+      t.integer :card_type, null: false      # enum: {credit: 0, debit: 1}     
+      
+      t.references :account, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
