bdb43f28a6f571a63a230fc280d32e5988d42669
FranVesnaver <fevesnaver@gmail.com>
2025-05-23 11:10:33 -0300

    Add bcrypt to server

--- server.rb
+++ server.rb
@@ -7,6 +7,7 @@ require_relative 'models/transaction'
 require 'sinatra'
 require 'bundler/setup'
 require 'sinatra/reloader' if Sinatra::Base.environment == :development
+require 'bcrypt'
 
 set :views, File.expand_path('views', __dir__)
 set :public_folder, File.expand_path('public', __dir__)
