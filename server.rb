ce92ead748c12a48d2c6c8dec1cddd36e9e1f27a
franBarosco <franbarosco@gmail.com>
2025-05-29 15:30:30 +0000

    New: Added a method for the login, and fixed problems with the layout
    Signed-off-by: franBarosco <franbarosco@gmail.com>

--- server.rb
+++ server.rb
@@ -20,14 +20,36 @@ class App < Sinatra::Application
   end
 
   get '/' do
-    erb :'/layout'
+    erb :'index'
+  end
+
+  get '/login' do
+    erb :'login'
+  end
+
+  post '/login' do
+    email = params[:email]
+    passwd = params[:password]
+    user = User.find_by(email: email)
+
+    if user && user.authenticate(password)
+      session[:user_id] = user_id
+      redirect '/dashboard'
+    else
+      @error = "Invalid email or password"
+      erb :login
+    end
   end
 
   get '/transfers' do
-    erb :'/transfers'
+    erb :'transfers'
   end
 
   get '/register' do
-    erb :'/register'
+    erb :'register'
+  end
+
+  get '/dashboard' do
+    erb :'dashboard'
   end
 end
