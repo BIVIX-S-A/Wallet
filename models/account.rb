ed68b99bc0c0ca9ba5269c5c2a852744c905f181
FranVesnaver <fevesnaver@gmail.com>
2025-05-30 13:51:30 -0300

    Fix: reorganizing the heads of the html files, fixing the connection between card and account

--- models/account.rb
+++ models/account.rb
@@ -1,5 +1,6 @@
 class Account < ActiveRecord::Base
   belongs_to :user
+  has_one :card
   has_many :source_transactions, class_name: 'Transaction', foreign_key: :source_account_id
   has_many :target_transactions, class_name: 'Transaction', foreign_key: :target_account_id
 end
